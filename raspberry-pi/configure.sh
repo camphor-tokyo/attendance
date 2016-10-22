c53a62a557936def5082b89e9e8788c334e7832c
Shotaro Kohama <khmshtr28@gmail.com>
2016-10-22 22:20:01 +0900

    Fix configure.sh

--- raspberry-pi/configure.sh
+++ raspberry-pi/configure.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-source=${SCRIPT_DIR}/conf/attendance.conf
+source=${SCRIPT_DIR}/static/attendance.conf
 dest=/etc/supervisor/conf.d/
 cp ${source} ${dest}
