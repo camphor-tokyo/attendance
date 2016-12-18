c3ac2a0c3978d8685151d828000cef7b59fce732
Shintaro Morikawa <sntr92@gmail.com>
2016-12-18 17:21:47 +0900

    wip

---
+++ attendance/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding:utf-8
+from __future__ import print_function
+
+import signal
+import time
+
+from attendance import Attendance
+
+
+def sigint_handler(self, *args):
+    print("\n[INFO] Recieve SIGINT. Stop notifier.")
+    exit(0)
+
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    attendance = Attendance()
+    attendance.daemon = True
+    attendance.start()
+
+    while True:
+        print("hoge")
+        time.sleep(1)
